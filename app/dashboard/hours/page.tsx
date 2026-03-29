807f7568fa236941eba8b7c6bc6537612182cf88
chicotemaa <chicote.matias@gmail.com>
2026-03-29 19:38:46 -0300

    fix: make hours dashboard dynamic

--- app/dashboard/hours/page.tsx
+++ app/dashboard/hours/page.tsx
@@ -2,6 +2,8 @@ import { getBusinessScheduleBundle } from "@/lib/business-data"
 
 import { HoursPageClient } from "./page-client"
 
+export const dynamic = "force-dynamic"
+
 export default async function HoursPage() {
   const { business, businessHours, bookingSettings, isLive } = await getBusinessScheduleBundle()
 
